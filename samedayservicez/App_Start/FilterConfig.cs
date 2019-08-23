b9e26210f3774099bb78780f73adb58d0cdf701b
Chris McClellan <christopher.mcclellan1@hotmail.com>
2019-08-22 19:31:42 -0500

    Add project files.

---
+++ samedayservicez/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace samedayservicez
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ samedayservicez/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="samedayservicez.MvcApplication" Language="C#" %>
