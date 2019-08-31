8f3e0fe4a1ed525ad9ef7141b73d906496475a40
Chris McClellan <christopher.mcclellan1@hotmail.com>
2019-08-31 13:32:00 -0500

    updated logo and added bootstrap

--- samedayservicez/App_Start/BundleConfig.cs
+++ samedayservicez/App_Start/BundleConfig.cs
@@ -23,8 +23,8 @@ namespace samedayservicez
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+           "~/Content/bootstrap.css",
+          "~/Content/site.css", "~/Content/theme.css", "~/Content/custom.css"));
         }
     }
 }
