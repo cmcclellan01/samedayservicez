d6742d285ba85c77e17008cb7276a297c30fda9f
Chris McClellan <christopher.mcclellan1@hotmail.com>
2019-09-08 12:24:10 -0500

    updated alot

--- samedayservicez/Controllers/ContractorController.cs
+++ samedayservicez/Controllers/ContractorController.cs
@@ -8,6 +8,10 @@ namespace samedayservicez.Controllers
 {
     public class ContractorController : Controller
     {
+
+
+      
+
         // GET: Contractor
         public ActionResult Index()
         {
@@ -21,20 +25,76 @@ namespace samedayservicez.Controllers
             return View();
         }
 
+
+        [HttpPost]
+        public ActionResult ContractorProfile(Models.ContractorRegistrationForm model)
+        {
+
+            // Get all states again
+            var states = samedayservicez.Utils.Extensions.GetStatesList();
+
+
+            model.States = GetSelectListItems(states);
+
+            // In case everything is fine - i.e. both "Name" and "State" are entered/selected,
+            // redirect user to the "Done" page, and pass the user object along via Session
+            if (ModelState.IsValid)
+            {
+                Session["ContractorSignupModel"] = model;
+                return RedirectToAction("ContractorProfileConfirm");
+            }
+
+            // Something is not right - so render the registration page again,
+            // keeping the data user has entered by supplying the model.
+            return View("ContractorProfile", model);
+        }
+
         public ActionResult ContractorProfile()
         {
-            ViewBag.Message = "Your ContractorProfile page.";
+            var states = samedayservicez.Utils.Extensions.GetStatesList();
 
-            return View();
+            var model = new Models.ContractorRegistrationForm();
+
+            model.States = GetSelectListItems(states);
+
+
+            ViewBag.Message = "Your ContractorProfile page.";
+          
+
+            return View(model);
         }
 
         public ActionResult ContractorProfileConfirm()
         {
             ViewBag.Message = "Your ContractorProfileConfirm page.";
 
-            return View();
+            var model = Session["ContractorSignupModel"] as Models.ContractorRegistrationForm;
+
+            // Display Done.html page that shows Name and selected state.
+            return View(model);
         }
-        
+
+        private IEnumerable<SelectListItem> GetSelectListItems(IEnumerable<SelectListItem> elements)
+        {
+            // Create an empty list to hold result of the operation
+            var selectList = new List<SelectListItem>();
+
+            // For each string in the 'elements' variable, create a new SelectListItem object
+            // that has both its Value and Text properties set to a particular value.
+            // This will result in MVC rendering each item as:
+            //     <option value="State Name">State Name</option>
+            foreach (var element in elements)
+            {
+                selectList.Add(new SelectListItem
+                {
+                    Value = element.Value,
+                    Text = element.Text
+                });
+            }
+
+            return selectList;
+        }
+
 
     }
 }
---
+++ samedayservicez/Models/ContractorRegForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace samedayservicez.Models
+{
+    public class ContractorRegistrationForm
+    {
+
+       
+        [Required(ErrorMessage = "Please Enter Your First Name",
+            AllowEmptyStrings = false)]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Your  Middle Name",
+          AllowEmptyStrings = false)]
+        [Display(Name = "Middle Name")]
+        public string MiddleName { get; set; }
+
+
+
+        [Required(ErrorMessage = "Please Enter Your Last Name",
+        AllowEmptyStrings = false)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Your Street Address",
+       AllowEmptyStrings = false)]
+        [Display(Name = "Address")]
+        public string Address { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Your City",
+       AllowEmptyStrings = false)]
+        [Display(Name = "City")]
+        public string City { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your State",
+         AllowEmptyStrings = false)]
+        [Display(Name = "State")]
+        public string State { get; set; }
+        public IEnumerable<SelectListItem> States { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Your Zip Code",
+       AllowEmptyStrings = false)]
+        [StringLength(5, MinimumLength = 5)]
+        [RegularExpression("(^\\d{5}(-\\d{4})?$)|(^[ABCEGHJKLMNPRSTVXY]{1}\\d{1}[A-Z]{1} *\\d{1}[A-Z]{1}\\d{1}$)", ErrorMessage = "Zip code is invalid.")] // US or Canada
+
+        [Display(Name = "Zip Code")]
+        public string ZipCode { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Your Phone Number",
+      AllowEmptyStrings = false)]
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Invalid Phone number")]
+        [Display(Name = "Phone Number")]
+        public string PhoneNumber { get; set; }
+
+
+        [Required(ErrorMessage ="Please Enter Email Address")]
+        [Display(Name = "Email")]
+        [RegularExpression(@"^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$",
+        ErrorMessage = "Please Enter Correct Email Address")]
+        public string Email { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter a Password",
+      AllowEmptyStrings = false)]
+        public string Password { get; set; }
+
+        [System.ComponentModel.DataAnnotations.Compare("Password", ErrorMessage = "Confirm password doesn't match, Try Again!")]
+        public string ConfirmPassword { get; set; }
+
+
+        [Required(ErrorMessage = "Choose date of birth")]
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        public DateTime? BirthDate { get; set; }
+
+
+        [Required(ErrorMessage = "Please Enter Your Bio",
+     AllowEmptyStrings = false)]
+        [Display(Name = "Bio")]
+        public string Bio { get; set; }
+
+    }
+}
